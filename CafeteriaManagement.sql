4afd4402172d0e4c8c2376cd86dee1ce8d02675c
Huy Nguyen <116279963+Senju14@users.noreply.github.com>
2024-05-01 16:55:56 +0700

    Delete Procedure, Just add Table Customer Loyalty

--- CafeteriaManagement.sql
+++ CafeteriaManagement.sql
@@ -11,74 +11,72 @@ GO
 -- Bảng lưu thông tin về danh mục sản phẩm
 CREATE TABLE ProductCategory
 (
-	CategoryID INT IDENTITY PRIMARY KEY,        -- ID của danh mục
-	CategoryName NVARCHAR (100) NOT NULL        -- Tên của danh mục
+    CategoryID INT IDENTITY PRIMARY KEY,        -- ID của danh mục
+    CategoryName NVARCHAR (100) NOT NULL        -- Tên của danh mục
 )
 GO
 
 -- Bảng lưu thông tin về sản phẩm
 CREATE TABLE Product
 (
-	ProductID INT IDENTITY PRIMARY KEY,          -- ID của sản phẩm
-	ProductName NVARCHAR (100) NOT NULL,        -- Tên của sản phẩm
-	CategoryID INT NOT NULL,                     -- ID của danh mục sản phẩm
-	Price FLOAT NOT NULL,                        -- Giá của sản phẩm
-	Status NVARCHAR(100) NOT NULL DEFAULT N'Available', -- Trạng thái của sản phẩm
-	FOREIGN KEY (CategoryID) REFERENCES dbo.ProductCategory(CategoryID) -- Khóa ngoại liên kết với bảng ProductCategory
+    ProductID INT IDENTITY PRIMARY KEY,          -- ID của sản phẩm
+    ProductName NVARCHAR (100) NOT NULL,        -- Tên của sản phẩm
+    CategoryID INT NOT NULL,                     -- ID của danh mục sản phẩm
+    Price FLOAT NOT NULL,                        -- Giá của sản phẩm
+    Status NVARCHAR(100) NOT NULL DEFAULT N'Available', -- Trạng thái của sản phẩm
+    FOREIGN KEY (CategoryID) REFERENCES dbo.ProductCategory(CategoryID) -- Khóa ngoại liên kết với bảng ProductCategory
 )
 GO
 
 -- Bảng lưu thông tin về mã giảm giá
 CREATE TABLE Discount
 (
-	DiscountID INT IDENTITY PRIMARY KEY,         -- ID của mã giảm giá
-	DiscountCode NVARCHAR (100) NOT NULL,        -- Mã giảm giá
-	Percentage FLOAT NOT NULL                    -- Phần trăm giảm giá
+    DiscountID INT IDENTITY PRIMARY KEY,         -- ID của mã giảm giá
+    DiscountCode NVARCHAR (100) NOT NULL,        -- Mã giảm giá
+    Percentage FLOAT NOT NULL                    -- Phần trăm giảm giá
 )
 GO
 
 -- Bảng lưu thông tin về khách hàng
 CREATE TABLE Customer
 (
-	CustomerID INT IDENTITY PRIMARY KEY,         -- ID của khách hàng
-	CustomerName NVARCHAR(100) NOT NULL,         -- Tên của khách hàng
-	PhoneNumber NVARCHAR(20) NOT NULL,           -- Số điện thoại của khách hàng
-	Points INT NOT NULL DEFAULT 0,               -- Điểm tích lũy của khách hàng
-	MembershipLevel NVARCHAR(100) NOT NULL DEFAULT N'Bronze' -- Hạng thành viên
+    CustomerID INT IDENTITY PRIMARY KEY,         -- ID của khách hàng
+    CustomerName NVARCHAR(100) NOT NULL,         -- Tên của khách hàng
+    PhoneNumber NVARCHAR(20) NOT NULL            -- Số điện thoại của khách hàng
 )
 GO
 
 -- Bảng lưu thông tin về đơn hàng
 CREATE TABLE OrderData
 (
-	OrderID INT IDENTITY PRIMARY KEY,            -- ID của đơn hàng
-	OrderDate DATE NOT NULL,                     -- Ngày đặt hàng
-	TotalAmount FLOAT NOT NULL,                  -- Tổng tiền
-	PaymentMethod NVARCHAR(100) NOT NULL,        -- Phương thức thanh toán
-	CustomerID INT,                              -- ID của khách hàng
-	FOREIGN KEY (CustomerID) REFERENCES dbo.Customer(CustomerID)  -- Khóa ngoại liên kết với bảng Customer
+    OrderID INT IDENTITY PRIMARY KEY,            -- ID của đơn hàng
+    OrderDate DATE NOT NULL,                     -- Ngày đặt hàng
+    TotalAmount FLOAT NOT NULL,                  -- Tổng tiền
+    PaymentMethod NVARCHAR(100) NOT NULL,        -- Phương thức thanh toán
+    CustomerID INT,                              -- ID của khách hàng
+    FOREIGN KEY (CustomerID) REFERENCES dbo.Customer(CustomerID)  -- Khóa ngoại liên kết với bảng Customer
 )
 GO
 
 -- Bảng lưu thông tin chi tiết của mỗi đơn hàng
 CREATE TABLE OrderDetail
 (
-	OrderDetailID INT IDENTITY PRIMARY KEY,       -- ID của chi tiết đơn hàng
-	OrderID INT NOT NULL,                         -- ID của đơn hàng
-	ProductID INT NOT NULL,                       -- ID của sản phẩm
-	Quantity INT NOT NULL,                        -- Số lượng
-	Price FLOAT NOT NULL,                         -- Giá tiền
-	FOREIGN KEY (OrderID) REFERENCES dbo.OrderData(OrderID),         -- Khóa ngoại liên kết với bảng OrderData
-	FOREIGN KEY (ProductID) REFERENCES dbo.Product(ProductID)   -- Khóa ngoại liên kết với bảng Product
+    OrderDetailID INT IDENTITY PRIMARY KEY,       -- ID của chi tiết đơn hàng
+    OrderID INT NOT NULL,                         -- ID của đơn hàng
+    ProductID INT NOT NULL,                       -- ID của sản phẩm
+    Quantity INT NOT NULL,                        -- Số lượng
+    Price FLOAT NOT NULL,                         -- Giá tiền
+    FOREIGN KEY (OrderID) REFERENCES dbo.OrderData(OrderID),         -- Khóa ngoại liên kết với bảng OrderData
+    FOREIGN KEY (ProductID) REFERENCES dbo.Product(ProductID)   -- Khóa ngoại liên kết với bảng Product
 )
 GO
 
 -- Bảng lưu thông tin về thẻ bàn
 CREATE TABLE TableCard
 (
-	TableCardID INT IDENTITY PRIMARY KEY,         -- ID của thẻ bàn
-	TableName NVARCHAR(100) NOT NULL,             -- Tên của thẻ bàn
-	Status NVARCHAR(100) NOT NULL DEFAULT N'Available'   -- Trạng thái của thẻ bàn
+    TableCardID INT IDENTITY PRIMARY KEY,         -- ID của thẻ bàn
+    TableName NVARCHAR(100) NOT NULL,             -- Tên của thẻ bàn
+    Status NVARCHAR(100) NOT NULL DEFAULT N'Available'   -- Trạng thái của thẻ bàn
 )
 GO
 
@@ -91,6 +89,16 @@ CREATE TABLE StatisticalData
     OrderMethod NVARCHAR(100) NOT NULL,         -- Phương thức đặt hàng: mang về, tại quán, shipper
     Total FLOAT NOT NULL,                       -- Doanh thu
     CustomerID INT                               -- ID của khách hàng
+);
+GO
+
+-- Bảng lưu thông tin về điểm và hạng thành viên của khách hàng
+CREATE TABLE CustomerLoyalty (
+    CustomerLoyaltyID INT IDENTITY PRIMARY KEY,
+    CustomerID INT NOT NULL,
+    Points INT NOT NULL DEFAULT 0,
+    MembershipLevel NVARCHAR(100) NOT NULL DEFAULT N'Bronze',
+    FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
 );
 GO
 
@@ -136,6 +144,13 @@ VALUES
     ('2024-04-18', '12:45:00', 'Shipping', 30.0, NULL), -- Changed CustomerID to NULL since no customer is associated with this order
     ('2024-04-19', '10:30:00', 'Take-away', 0.0, NULL); -- Change the last value accordingly
 
+-- Thêm dữ liệu vào bảng CustomerLoyalty
+INSERT INTO CustomerLoyalty (CustomerID, Points, MembershipLevel)
+VALUES
+    (1, 50, N'Silver'),
+    (2, 200, N'Gold'),
+    (3, 800, N'Diamond');
+
 -- TEST
 SELECT * FROM ProductCategory;
 SELECT * FROM Product;
@@ -144,33 +159,15 @@ SELECT * FROM Customer;
 SELECT * FROM OrderData;
 SELECT * FROM OrderDetail;
 SELECT * FROM StatisticalData;
+SELECT * FROM CustomerLoyalty;
 
 GO
 
--- Customer Loyalty
-CREATE PROCEDURE UpdateMembershipLevel
-    @CustomerID INT
-AS
-BEGIN
-    DECLARE @Points INT
-    SELECT @Points = Points FROM Customer WHERE CustomerID = @CustomerID
-
-    IF @Points < 100
-        UPDATE Customer SET MembershipLevel = N'Bronze' WHERE CustomerID = @CustomerID
-    ELSE IF @Points >= 100 AND @Points < 500
-        UPDATE Customer SET MembershipLevel = N'Silver' WHERE CustomerID = @CustomerID
-    ELSE IF @Points >= 500 AND @Points < 1000
-        UPDATE Customer SET MembershipLevel = N'Gold' WHERE CustomerID = @CustomerID
-    ELSE
-        UPDATE Customer SET MembershipLevel = N'Diamond' WHERE CustomerID = @CustomerID
-END
-GO
-
--- Call Procedure
-DECLARE @CustomerID INT
-SELECT @CustomerID = CustomerID FROM OrderData WHERE OrderID = SCOPE_IDENTITY()
-EXEC UpdateMembershipLevel @CustomerID
-
--- TEST Procedure
-SELECT CustomerName, PhoneNumber, Points, MembershipLevel 
-FROM Customer
+-- Câu truy vấn để lấy thông tin khách hàng và hạng thành viên
+SELECT 
+    c.CustomerName, 
+    c.PhoneNumber, 
+    cl.Points, 
+    cl.MembershipLevel
+FROM Customer c
+JOIN CustomerLoyalty cl ON c.CustomerID = cl.CustomerID;
