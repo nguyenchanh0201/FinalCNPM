1d697e5743d910eccbf6cc608df7510ee82f9485
nguyenchanh0201 <160260455+nguyenchanh0201@users.noreply.github.com>
2024-05-12 02:05:56 +0700

    Add files via upload

---
+++ BLL/BLLRole.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DAL;
+using DTO;
+namespace BLL
+{
+    public  class BLLRole
+    {
+        static DALRole dalRole;
+
+        public BLLRole(int roleID, String roleName)
+        {
+            dalRole = new DALRole(roleID, roleName);
+        }
+
+        public void insert(String  roleName)
+        {
+            dalRole.insert(roleName);
+        }
+
+        public static void update(int roleID, String roleName)
+        {
+            if (dalRole == null)
+            {
+                dalRole = new DALRole(0, "");
+            }
+            dalRole.update(roleID, roleName);
+        }
+
+        public static void delete(int roleID)
+        {
+            if (dalRole == null)
+            {
+                dalRole = new DALRole(0, "");
+            }
+            dalRole.delete(roleID);
+        }
+
+        public static List<Role> getRole()
+        {
+            if (dalRole == null)
+            {
+                dalRole = new DALRole(0, "");
+            }
+            return dalRole.getRole();
+        }
+        public static DataTable select()
+        {
+            if (dalRole == null)
+            {
+                dalRole = new DALRole(0, "");
+            }
+            return dalRole.select();
+        }
+       
+    }
+}
---
+++ BLL/BLLShift.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using DAL; 
+
+namespace BLL
+{
+    public class BLLShift
+    {
+        static DALShift dalshift;
+
+        public BLLShift(String shiftID, DateTime shiftStart, DateTime shiftEnd, string username)
+        {
+            dalshift = new DALShift(shiftID, shiftStart, shiftEnd, username);
+        }
+
+        public  string createShiftID()
+        {
+            if (dalshift == null)
+            {
+                DALShift dalshift = new DALShift("", DateTime.Now, DateTime.Now, "");
+            }
+            return dalshift.createShiftID();
+        }   
+
+        public void addShift(DateTime shiftStart, DateTime shiftEnd, string username)
+        {
+            if (dalshift == null)
+            {
+                DALShift dalshift = new DALShift("", DateTime.Now, DateTime.Now, "");
+            }
+            dalshift.addShift(shiftStart, shiftEnd, username);
+        }
+
+        public void updateShift(String shiftID, DateTime shiftEnd)
+        {
+            if (dalshift == null)
+            {
+                DALShift dalshift = new DALShift("", DateTime.Now, DateTime.Now, "");
+            }
+            dalshift.updateShift(shiftID, shiftEnd);
+        }
+    }
+}
