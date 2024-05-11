9df629c27d3778576a3e27a7f9c241d1243e98e9
nguyenchanh0201 <chanhnguyen34@gmail.com>
2024-05-11 12:54:59 +0700

    update Customer ver2

--- BLL/BLLCustomer.cs
+++ BLL/BLLCustomer.cs
@@ -4,16 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DTO;
-using DAL; 
+using DAL;
+using System.Security.Cryptography;
 
 namespace BLL
 {
     public class BLLCustomer
     {
         DALCustomer dalCustomer;
-        public BLLCustomer(String customerId, String customerName, String phoneNumber, string gender, int points, int rankID)
+        public BLLCustomer(int customerId, String customerName, String phoneNumber, string gender, int points, int rankID)
         {
             dalCustomer = new DALCustomer(customerId, customerName, phoneNumber, gender, points, rankID);
         }
+
+        public string createCustomerID()
+        {
+            return dalCustomer.createCustomerID();
+        }
+
+        public void insert(String customerName, String phoneNumber, String gender, int points, int rankID)
+        {
+            if  (dalCustomer == null)
+            {
+                dalCustomer = new DALCustomer(0, "", "", "", 0, 0);
+            }
+            dalCustomer.insert(customerName, phoneNumber, gender, points, rankID);
+        }
+
+
     }
 }
